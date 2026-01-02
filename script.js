a133fbee9150302e7d4fedfd459ba75375c84ebb
navruzbaevrahim-ux <navruzbaevrahim@gmail.com>
2026-01-02 22:19:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function showPage(id) {
+    document.getElementById('home').classList.add('hidden');
+    document.getElementById('player').classList.add('hidden');
+    document.getElementById(id).classList.remove('hidden');
+}
+
+function openAnime(title, url) {
+    const video = document.getElementById('main-video');
+    document.getElementById('video-src').src = url;
+    video.load();
+    showPage('player');
+}
 
